a1f6fe9137b7c8daab64dc7289b66ac03a9b9c8a
ricvigi <ricvigi@proton.me>
2025-03-26 15:53:14 +0100

    Still working on find_best_block_size.cpp. Now i get the best block size and also ellCols, the number of columns in the BELL representation. This function is turning into a complete function to get all parameters needed for BELL conversion

--- programs/find_best_block_size.cpp
+++ programs/find_best_block_size.cpp
@@ -4,6 +4,13 @@
 #include <cmath>
 #include "myHeaders.hpp"
 
+int bestBlockSize(torch::Tensor& A, /* in */
+                  int x,            /* in */
+                  int y,            /* in */
+                  int& res,         /* out */
+                  int& ellCols);    /* out */
+
+
 int main(int argc, char** argv)
 {
   if (argc < 3)
@@ -12,69 +19,110 @@ int main(int argc, char** argv)
     fflush(stdout);
     return EXIT_FAILURE;
   }
-  int x, y, kernelSize, nBlocksH, nBlocksW, zeroBlocks, maxZeroBlocks, res, zeroCount, temp;
-  float threshold, start, end;
+  int x, y, res, ellCols;
+  float threshold;
   torch::Tensor A, B, bSums;
 
   x = atoi(argv[1]);
   y = atoi(argv[2]);
   threshold = atof(argv[3]);
-  kernelSize = 2;
+
+  A = torch::randn({x, y});
+  A.masked_fill_(A < threshold, 0);
+  bestBlockSize(A, x, y, res, ellCols);
+  printf("RES: %d\n", res);
+  printf("ELLCOLS: %d\n", ellCols);
+
+
+  printf("All done. Great Success!\n");
+  return EXIT_SUCCESS;
+}
+
+/**
+ * @brief Find the block size that filters out the most zeroes
+ *
+ * @param &A Reference to sparse tensor that we want to convert to BELL
+ * @param x Size of the first dimension of A
+ * @param y Size of the second dimension of A
+ * @param &res Reference to the variable that will store the best block size for tensor A
+ * @param &ellCols Reference to the variable that will store the number of columns in BELL
+ *
+ * @return Best block size on success, EXIT_FAILURE on error.
+ */
+int bestBlockSize(torch::Tensor& A, int x, int y, int& res, int& ellCols)
+{
+  int i, kernelSize, nBlocksH, nBlocksW, zeroBlocks, maxZeroBlocks, zeroCount, nZeroes;
+  float start, end;
+  torch::Tensor B, bSums;
   if (x != y)
   {
     printf("We are only accepting square matrices for now...\n");
     fflush(stdout);
     return EXIT_FAILURE;
   }
-
-  A = torch::randn({x, y});
-  A.masked_fill_(A < threshold, 0);
-  // This matrix is used to test the ability of the algorithm to prefer larger kernel sizes
-  // A = torch::tensor({
-  //       {0, 0, 0, 0, 1, 1, 1, 1},
-  //       {0, 0, 0, 0, 1, 1, 1, 1},
-  //       {0, 0, 0, 0, 1, 1, 1, 1},
-  //       {0, 0, 0, 0, 1, 1, 1, 1},
-  //       {1, 1, 1, 1, 0, 0, 0, 0},
-  //       {1, 1, 1, 1, 0, 0, 0, 0},
-  //       {1, 1, 1, 1, 0, 0, 0, 0},
-  //       {1, 1, 1, 1, 0, 0, 0, 0}
-  //   });
   res = 0;
   zeroCount = 0;
   maxZeroBlocks = 0;
+  std::vector<int> divisors = findDivisors(x);
 
-  start = omp_get_wtime();
-  do
+  printTensor(A, x, y);
+
+  /* Lambdas */
+  auto computeZeroBlocks = [&](int kernelSize) -> int
   {
     printf("Kernel size: %d\n", kernelSize);
+
+    int nBlocksH = x / kernelSize;
+    int nBlocksW = y / kernelSize;
+
+    torch::Tensor B = A.view({nBlocksH, kernelSize, nBlocksW, kernelSize});
+    B = B.permute({0, 2, 1, 3});
+    torch::Tensor bSums = B.sum({2, 3});
+
+    return (bSums == 0).sum().item<int>();
+  };
+  auto computeEllCols = [&](int kernelSize) -> int
+  {
     nBlocksH = x / kernelSize;
     nBlocksW = y / kernelSize;
     B = A.view({nBlocksH, kernelSize, nBlocksW, kernelSize});
     B = B.permute({0, 2, 1, 3});
     bSums = B.sum({2, 3});
-    zeroBlocks = (bSums == 0).sum().item<int>();
-    temp = zeroBlocks * kernelSize * kernelSize; /* ATTENTION: To correctly compute the number of zeroes you are filtering out, you have to multiply the number of zero blocks with the number of elements in the kernel! This is because bigger kernels will tend to have less zero blocks than smaller ones, but might actually filter out more zeroes! */
-    if (zeroCount <= temp) /* <= condition should ensure that we always select a bigger kernel size if two sizes yield the same results. ATTENTION: check if this is actually true */
+    printTensor(bSums, nBlocksH, nBlocksW);
+    return (bSums != 0).sum(1).max().item<int>();
+  };
+
+  /* Get the optimal kernelSize value */
+  start = omp_get_wtime();
+  for (i = 0; i < divisors.size(); i++)
+  {
+    kernelSize = divisors[i];
+    zeroBlocks = computeZeroBlocks(kernelSize);
+    if (zeroBlocks == 0)
+    {
+      break;
+    }
+    nZeroes = zeroBlocks * kernelSize * kernelSize;
+    if (zeroCount <= nZeroes)
     {
       maxZeroBlocks = zeroBlocks;
-      zeroCount = temp;
+      zeroCount = nZeroes;
       res = kernelSize;
     }
-    printf("We can filter out %d zeroes with a kernel of size %d\n", temp, kernelSize);
+    printf("We can filter out %d zeroes with a kernel of size %d\n", nZeroes, kernelSize);
     printf("Matrix has %d zero blocks of size %d\n", zeroBlocks, kernelSize);
 
-    kernelSize *= 2;
-  } while (kernelSize <= floor(x / 2));
+  }
+
+  /* Now get ellCols, the actual number of columns in the BELL format */
+  /* TODO: Enclose this operation in a function */
+  ellCols = computeEllCols(res);
 
   end = omp_get_wtime();
-
 
   printf("We can filter out %d zeroes with a kernel of size %d\n", zeroCount, res);
   printf("Matrix has %d zero blocks of size %d\n", maxZeroBlocks, res);
   printf("Total time needed for computation: %f\n", end - start);
 
-
-  printf("All done. Great Success!\n");
   return EXIT_SUCCESS;
 }
--- programs/myHeaders.hpp
+++ programs/myHeaders.hpp
@@ -1,7 +1,11 @@
 #ifndef MY_HEADERS_HPP
 #define MY_HEADERS_HPP
+#include <torch/torch.h>
+#include <cstdio>
+#include <vector>
+#include <cmath>
 
-static inline void print_tensor(const torch::Tensor &A, int x, int y)
+static inline void printTensor(const torch::Tensor &A, int x, int y)
 {
   for (int i = 0; i < x; i++)
   {
@@ -11,6 +15,22 @@ static inline void print_tensor(const torch::Tensor &A, int x, int y)
     }
     printf("\n");
   }
+}
+
+/* ATTENTION: Find a better way to implement this algorithm. NOTE: keep in mind that we need to return a sorted list.
+ * The algorithm returns all divisors of x up to x / 2 */
+static inline std::vector<int> findDivisors(int x) {
+    std::vector<int> divisors;
+    int i = 2;
+    while (i <= (x / 2)) /* With normal iteration the result array is naturally sorted */
+    {
+      if (x % i == 0)
+      {
+        divisors.push_back(i);
+      }
+      i++;
+    }
+    return divisors; /* ATTENTION: Must return a sorted array */
 }
 
 #endif
