8701da8fefd94d3796b9b17ef2496aadcd221831
ricvigi <ricvigi@proton.me>
2025-08-04 22:19:43 +0200

    building for python

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -8,6 +8,12 @@ std::tuple<torch::Tensor, BellMetadata> to_sparse_blockedell(torch::Tensor &dens
     return to_sparse_blockedell_impl<float>(dense);
 }
 
+torch::Tensor to_sparse_blockedell_temp(torch::Tensor& dense)
+{
+    return to_sparse_blockedell_impl<float>(dense).first;
+}
+
+
 TORCH_LIBRARY(my_sparse, m)
 {
     m.def("to_sparse_blockedell(Tensor self) -> (Tensor, __torch__.my_sparse.BellMetadata)");
@@ -20,17 +26,22 @@ TORCH_LIBRARY(my_sparse, m)
 // }
 
 // Python binding
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    pybind11::class_<BellMetadata>(m, "BellMetadata")
-        .def_readonly("ellBlockSize", &BellMetadata::ellBlockSize)
-        .def_readonly("ellCols", &BellMetadata::ellCols)
-        .def_readonly("nnz", &BellMetadata::nnz)
-        .def_readonly("size", &BellMetadata::size)
-        .def_readonly("ellColInd", &BellMetadata::ellColInd)
-        .def_readonly("ellValue", &BellMetadata::ellValue);
-
-    m.def("to_sparse_blockedell", &to_sparse_blockedell, "Convert to BlockedELL format");
+// PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+//     pybind11::class_<BellMetadata>(m, "BellMetadata")
+//         .def_readonly("ellBlockSize", &BellMetadata::ellBlockSize)
+//         .def_readonly("ellCols", &BellMetadata::ellCols)
+//         .def_readonly("nnz", &BellMetadata::nnz)
+//         .def_readonly("size", &BellMetadata::size)
+//         .def_readonly("ellColInd", &BellMetadata::ellColInd)
+//         .def_readonly("ellValue", &BellMetadata::ellValue);
+//
+//     m.def("to_sparse_blockedell", &to_sparse_blockedell, "Convert to BlockedELL format");
+// }
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
+    m.def("to_sparse_blockedell", &to_sparse_blockedell_temp, "Convert to BlockedELL format");
 }
+
 
 
 
