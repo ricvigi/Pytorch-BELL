e52210b0d55cb22c2ad924d91ef79c5721a35dd4
ricvigi <ricvigi@proton.me>
2025-08-04 22:12:15 +0200

    building for python

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
+extra_compile_args = {
+    'cxx': ['-O3', '-std=c++17', '-D_GLIBCXX_USE_CXX11_ABI=0'],
+    'nvcc': ['-O3', '--expt-relaxed-constexpr']
+}
+
 setup(
     name='blockedell',
     ext_modules=[
         CUDAExtension(
             name='blockedell',
             sources=['bindings.cpp', 'functions.cu'],
-            extra_compile_args={'cxx': [], 'nvcc': ['-O3']}
+            extra_compile_args=extra_compile_args
         )
     ],
     cmdclass={'build_ext': BuildExtension}
