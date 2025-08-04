8a848c16eb74982a8d033c0148a068901b97b270
ricvigi <ricvigi@proton.me>
2025-08-04 22:31:12 +0200

    building for python

--- src/setup.py
+++ src/setup.py
@@ -1,18 +1,31 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-extra_compile_args = {
-    'cxx': ['-O3', '-std=c++17', '-D_GLIBCXX_USE_CXX11_ABI=0'],
-    'nvcc': ['-O3', '--expt-relaxed-constexpr', '-Wno-deprecated-gpu-targets', '-Ilibtorch/include -Ilibtorch/include/torch/csrc/api/include', '-ltorch -ltorch_cpu -lc10', '-Xcompiler -fopenmp -I/opt/cuda/include -Ilibtorch/include -Ilibtorch/include/torch/csrc/api/include -Llibtorch/lib -Xlinker -rpath -Xlinker libtorch/lib -ltorch -ltorch_cpu -lc10 -lcusparse']
-}
-
 setup(
     name='blockedell',
     ext_modules=[
         CUDAExtension(
             name='blockedell',
             sources=['bindings.cpp', 'functions.cu'],
-            extra_compile_args=extra_compile_args
+            include_dirs=[
+                # Torch headers
+                f"{os.environ['CONDA_PREFIX']}/lib/python3.10/site-packages/torch/include",
+                f"{os.environ['CONDA_PREFIX']}/lib/python3.10/site-packages/torch/include/torch/csrc/api/include",
+                "/cm/shared/apps/linux-ubuntu22.04-zen2/cuda/11.8.0/include"
+            ],
+            library_dirs=[
+                f"{os.environ['CONDA_PREFIX']}/lib/python3.10/site-packages/torch/lib",
+                "/cm/shared/apps/linux-ubuntu22.04-zen2/cuda/11.8.0/lib64"
+            ],
+            libraries=['torch', 'torch_cpu', 'c10', 'cudart', 'c10_cuda', 'torch_cuda'],
+            extra_compile_args={
+                'cxx': ['-O3', '-std=c++17', '-D_GLIBCXX_USE_CXX11_ABI=0'],
+                'nvcc': [
+                    '-O3', '--expt-relaxed-constexpr',
+                    '-gencode=arch=compute_80,code=sm_80',
+                    '-D_GLIBCXX_USE_CXX11_ABI=0'
+                ]
+            }
         )
     ],
     cmdclass={'build_ext': BuildExtension}
