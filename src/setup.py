1b7bec646d5d6297734b954af4fd7bab2f914522
ricvigi <ricvigi@proton.me>
2025-08-04 22:29:31 +0200

    building for python

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 extra_compile_args = {
     'cxx': ['-O3', '-std=c++17', '-D_GLIBCXX_USE_CXX11_ABI=0'],
-    'nvcc': ['-O3', '--expt-relaxed-constexpr']
+    'nvcc': ['-O3', '--expt-relaxed-constexpr', '-Wno-deprecated-gpu-targets', '-Ilibtorch/include -Ilibtorch/include/torch/csrc/api/include', '-ltorch -ltorch_cpu -lc10', '-Xcompiler -fopenmp -I/opt/cuda/include -Ilibtorch/include -Ilibtorch/include/torch/csrc/api/include -Llibtorch/lib -Xlinker -rpath -Xlinker libtorch/lib -ltorch -ltorch_cpu -lc10 -lcusparse']
 }
 
 setup(
